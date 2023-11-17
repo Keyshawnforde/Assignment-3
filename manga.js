e317017d00712f406f1f3ef7fb61be5874de8cee
Keyshawn Forde <145371554+Keyshawnforde@users.noreply.github.com>
2023-11-16 23:08:44 -0500

    Add files via upload

--- manga.js
+++ manga.js
@@ -1,10 +1,9 @@
-// models/manga.js
 const mongoose = require('mongoose');
 
 const mangaSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  volumes: { type: String, required: true },
-  finished: { type: String, required: true },
+  name: { type: String, required: true }, //Makes it so only strings can be entered
+  volumes: { type: Number, required: true }, //Makes it so only whole numbers can be entered
+  finished: { type: String, required: true }, //Makes it so only strings can be entered
 });
 
 const Manga = mongoose.model('Manga', mangaSchema);
